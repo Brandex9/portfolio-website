02c275003135d63ca79cd12ffec95ca6e6e6042d
Brandon Extra <brandex@Brandons-MacBook-Air.local>
2022-09-15 17:18:50 -0400

    deploy

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Brandon Extra</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
