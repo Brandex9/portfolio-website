c096d2386aeda53736add1bb76c8c13b91a1fe89
Brandon Extra <brandex@Brandons-MacBook-Air.local>
2022-09-17 19:32:50 -0400

    about styling

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./About.css";
 import ME from "../../assets/me-about.jpg";
+import { FaMedal } from "react-icons/fa";
+import { AiOutlineProject } from "react-icons/ai";
 
 const About = () => {
   return (
@@ -15,7 +17,36 @@ const About = () => {
           </div>
         </div>
 
-        <div className="about__content"></div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaMedal className="about__icon" />
+              <h5>Experience</h5>
+              <small>placeholder years</small>
+            </article>
+            <article className="about__card">
+              <AiOutlineProject className="about__icon" />
+              <h5>Projects</h5>
+              <small>6</small>
+            </article>
+            <article className="about__card">
+              <FaMedal className="about__icon" />
+              <h5>Experience</h5>
+              <small>placeholder years</small>
+            </article>
+          </div>
+
+          <p>
+            Software developer passionate about exploring ways technology can
+            provide practical solutions to everyday problems. Here I explore
+            different programming languages and utilize the knowledge gained to
+            make simple projects.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
       </div>
     </section>
   );
