984269156aa808fbbe95e422476fed4da9d1388b
Brandon Extra <brandex@Brandons-MacBook-Air.local>
2022-09-18 20:08:32 -0400

    styling portfolio

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./experience.css";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>What Skills I use</h5>
+      <h2>My Experience</h2>
+
+      <div></div>
+    </section>
+  );
 };
 
 export default Experience;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from "../../assets/me-about.jpg";
 
 function Portfolio() {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="minesweeper" />
+          </div>
+          <div className="portfolio__item-cta">
+            <h3>Minesweeper</h3>
+            <a
+              href="https://github.com/Culossus/Minesweeper"
+              className="btn"
+              target="_blank"
+            >
+              Github link
+            </a>
+            <a
+              href="https://culossus.github.io/Minesweeper/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio;
