6b5dbeaa8c876ff54c4a84aac782e53d2d3abd70
Brandon Extra <brandex@Brandons-MacBook-Air.local>
2022-12-10 14:49:05 -0500

    trying local storage dark mode

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import useLocalStorage from "use-local-storage";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
@@ -8,8 +9,23 @@ import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 
 const app = () => {
+  const defaultDark = window.matchMedia(`(prefers-color-scheme: dark)`).matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
   return (
     <>
+      <div className="app-light" data-theme={theme}>
+        <span>Easy Darkmode and themes in react</span>
+        <button className="test-light" onClick={switchTheme}>
+          Switch to {theme === "light" ? "Dark" : "Light"} Theme
+        </button>
+      </div>
       <Header />
       <Nav />
       <About />
